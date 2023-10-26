724e64133fc5cdcacd2d6c76080b98eb35fc1734
PMatthaei <pmd.matthaei@gmail.com>
2023-10-27 00:01:02 +0200

    Switched to dynamic private for runtime env variable "ALLOWED_EMAIL"

--- src/lib/auth.guard.ts
+++ src/lib/auth.guard.ts
@@ -1,9 +1,9 @@
 import {redirect} from "@sveltejs/kit";
 import type {Session} from "@auth/core/types";
-import {ALLOWED_EMAIL} from "$env/static/private";
+import {env} from "$env/dynamic/private";
 
 export const guard = (session: Session | null) => {
-    if (session?.user?.email !== ALLOWED_EMAIL) {
+    if (session?.user?.email !== env.ALLOWED_EMAIL) {
         throw redirect(307, 'auth/signin');
     }
     if (!session) {
