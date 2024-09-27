1e024abc8ed3756a6f002ce0897bd9330377dec4
PMatthaei <pmd.matthaei@gmail.com>
2024-09-28 00:17:42 +0200

    Improvements

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: 'sveltekit-app',
+            script: 'build/index.js', // Replace with the entry point of your SvelteKit app or server
+            instances: 1,           // You can increase this for a clustered setup
+            exec_mode: 'fork',      // You can change this to 'cluster' if needed
+            node_args: '-r dotenv/config',
+            env: {                  // Environment variables for production
+                NODE_ENV: 'production',
+                PORT: 8080            // Set the port your app will run on
+            },
+            env_development: {      // Environment variables for development
+                NODE_ENV: 'development',
+                PORT: 3000            // Different port for development environment
+            }
+        }
+    ]
+};
