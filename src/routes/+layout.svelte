99f6125b677053f5f3bdd1d3c66b9cd0fd0f7a4b
PMatthaei <pmd.matthaei@gmail.com>
2024-09-27 23:51:34 +0200

    Improvements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,12 +52,6 @@
             </li>
 
             <li>
-                <div class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group">
-                    <img alt="Sveltekit - Prain" class="h-10 w-auto" src="{logo}">
-                    <span class="ml-4 text-2xl font-bold">Prain</span>
-                </div>
-            </li>
-            <li>
                 <a aria-current={$page.url.pathname === '/moti' ? 'page' : undefined} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                    href="/moti">
                     <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -66,29 +60,6 @@
                     <span class="ml-3">Music Of The Internet</span>
                 </a>
             </li>
-            <li>
-                <a aria-current={$page.url.pathname === '/connect' ? 'page' : undefined} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                   href="/connect">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-800 dark:text-white"
-                         fill="none" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 1v16M1 9h16" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                              stroke-width="2"/>
-                    </svg>
-                    <span class="ml-3">Build Braincell</span>
-                </a>
-            </li>
-
-            <li>
-                <a aria-current={$page.url.pathname === '/query' ? 'page' : undefined} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                   href="/query">
-                    <svg aria-hidden="true" class="w-5 h-5 text-gray-800 dark:text-white"
-                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm0 16a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm1-5.034V12a1 1 0 0 1-2 0v-1.418a1 1 0 0 1 1.038-.999 1.436 1.436 0 0 0 1.488-1.441 1.501 1.501 0 1 0-3-.116.986.986 0 0 1-1.037.961 1 1 0 0 1-.96-1.037A3.5 3.5 0 1 1 11 11.466Z"/>
-                    </svg>
-                    <span class="ml-3">Query Braincell</span>
-                </a>
-            </li>
-
 
         </ul>
     </div>
--- src/routes/moti/+page.svelte
+++ src/routes/moti/+page.svelte
@@ -48,7 +48,7 @@
         if (synth) {
             synth.disconnect();
         }
-        if(eventSource){
+        if (eventSource) {
             eventSource.close();
         }
     });
@@ -84,8 +84,36 @@
         </button>
     </div>
 
-    <button on:click={toggleMute}>{isMuted ? 'Unmute' : 'Mute'}</button>
-    <button on:click={start}>Play</button>
+    <div class="flex flex-row gap-4 justify-center">
+        <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                on:click={toggleMute}>
+            {isMuted ? 'Unmute' : 'Mute'}
+            {#if isMuted}
+                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M5.707 4.293a1 1 0 0 0-1.414 1.414l14 14a1 1 0 0 0 1.414-1.414l-.004-.005C21.57 16.498 22 13.938 22 12a9.972 9.972 0 0 0-2.929-7.071 1 1 0 1 0-1.414 1.414A7.972 7.972 0 0 1 20 12c0 1.752-.403 3.636-1.712 4.873l-1.433-1.433C17.616 14.37 18 13.107 18 12c0-1.678-.69-3.197-1.8-4.285a1 1 0 1 0-1.4 1.428A3.985 3.985 0 0 1 16 12c0 .606-.195 1.335-.59 1.996L13 11.586V6.135c0-1.696-1.978-2.622-3.28-1.536L7.698 6.284l-1.99-1.991ZM4 8h.586L13 16.414v1.451c0 1.696-1.978 2.622-3.28 1.536L5.638 16H4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2Z"/>
+                </svg>
+
+            {:else}
+                <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.5 8.43A4.985 4.985 0 0 1 17 12c0 1.126-.5 2.5-1.5 3.5m2.864-9.864A8.972 8.972 0 0 1 21 12c0 2.023-.5 4.5-2.5 6M7.8 7.5l2.56-2.133a1 1 0 0 1 1.64.768V12m0 4.5v1.365a1 1 0 0 1-1.64.768L6 15H4a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1m1-4 14 14"/>
+                </svg>
+            {/if}
+        </button>
+
+        <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                on:click={start}>
+            Play
+            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
+                      clip-rule="evenodd"/>
+            </svg>
+
+        </button>
+    </div>
+
     <canvas id="visualizer" width="800" height="300"></canvas>
 
 </div>
