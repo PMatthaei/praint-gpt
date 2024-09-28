ff5b4aba38221919dabdb53cb2ec4c52cbd327ad
PMatthaei <pmd.matthaei@gmail.com>
2024-09-28 10:09:04 +0200

    Improvements

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import { ViteDevServer, defineConfig } from 'vite'
 import {webSocketServer} from "./server/websocket-server";
 
 export default defineConfig({
-	plugins: [sveltekit(), webSocketServer]
+	plugins: [sveltekit(), webSocketServer],
+	build: {
+		target: 'esnext', // or 'modules'
+		outDir: 'build',
+		rollupOptions: {
+			output: {
+				format: 'esm', // Ensures the output is ESM
+			},
+		},
+	},
 })
