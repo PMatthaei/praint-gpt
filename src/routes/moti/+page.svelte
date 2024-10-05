c7d8d5c73477274fa9d6101645f6e07b590f132c
PMatthaei <pmd.matthaei@gmail.com>
2024-10-05 11:19:27 +0200

    Improvements

--- src/routes/moti/+page.svelte
+++ src/routes/moti/+page.svelte
@@ -156,7 +156,7 @@
 
 <div class="flex flex-col gap-4 h-screen p-4 sm:ml-64">
 
-    <div class="absolute animate-fade-in z-10 rounded-lg min-w-80 bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+    <div class="hidden absolute animate-fade-in z-10 rounded-lg min-w-80 bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
         <p class="font-bold">Channel not available</p>
         <p>{ errorMessage ?? '' }</p>
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
@@ -164,7 +164,19 @@
         </span>
     </div>
 
-    <div id="audio-motion-container" class="animate-fade-in"></div>
+    <div class="flex flex-row relative">
+        <div id="audio-motion-container" class="animate-fade-in flex-grow">
+
+        </div>
+        <div class="absolute flex-row flex bottom-0 left-1/2">
+            <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clip-rule="evenodd"/>
+            </svg>
+            <span class="text-white font-bold">0</span>
+        </div>
+
+    </div>
+
 
     <div class="flex flex-row gap-4 justify-center">
         {#if activeChannel}
